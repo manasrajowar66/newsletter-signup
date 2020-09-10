78bdb70fb2e8e8266b74554fcbc637a5c19b6e70
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2020-09-10 15:28:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+  var fname=req.body.firstName;
+  var lname=req.body.lastName;
+  var email=req.body.email;
+  var data={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:fname,
+          LNAME:lname
+        }
+      }
+    ]
+  };
+  var jsondata=JSON.stringify(data);
+  var url="https://us17.api.mailchimp.com/3.0/lists/521f6032ea/";
+  var options={
+    method:"POST",
+    auth:"Manas1:3e2f49af914dff8d4d76d0a3a07c95bb-us17"
+  };
+  var request=https.request(url,options,function(responce){
+      if(responce.statusCode===200)
+      res.sendFile(__dirname+"/success.html");
+      else
+      res.sendFile(__dirname+"/failure.html");
+  });
+  request.write(jsondata);
+  request.end();
+});
+
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server running at port no 3000");
+});
+
+
+// api key
+// 541248d71cecc3652aa7e59ebcf2c7df-us17
+
+// list id
+// 521f6032ea
